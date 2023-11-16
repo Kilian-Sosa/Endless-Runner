84d29cb04439d977a055e090eaea7f18db637b1d
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2023-11-16 19:13:46 +0000

    feat: Add parallax movement

---
+++ Assets/Scripts/ParallaxMotion.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ParallaxMotion : MonoBehaviour {
+
+    [SerializeField] float parallaxSpeed;
+    
+    void Update() {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            GetComponent<SpriteRenderer>().material.mainTextureOffset += Vector2.right * parallaxSpeed * Time.deltaTime;
+    }
+}
